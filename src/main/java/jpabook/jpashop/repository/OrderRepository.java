393fc2954a7d597aaaf2f419047f03a7b2e926d4
Jaehyuk <jaehyuk.pyon@gmail.com>
2022-11-14 08:03:08 +0900

    Thymeleaf를 사용한 화면단 - 회원가입(사용자 등록)

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
 import java.util.List;
 
 @Repository
@@ -21,8 +22,22 @@ public class OrderRepository {
         return em.find(Order.class, id);
     }
 
-//    public List<Order> findAll(OrderSearch orderSearch) {
-//
-//    }
+    public List<Order> findAllByString(OrderSearch orderSearch) {
+        String query = "SELECT o " +
+                "FROM Order o join o.member m " +
+                "WHERE o.status = :status " +
+                "AND m.name LIKE :name";
+
+        List<Order> resultList = em.createQuery(query, Order.class)
+                .setParameter("status", orderSearch.getOrderStatus())
+                .setParameter("name", orderSearch.getMemberName())
+                .getResultList();
+
+        return resultList;
+    }
+
+    public List<Order> findAllByCriteria(OrderSearch orderSearch) {
+        return null;
+    }
 
 }
